aa817b0ffa46679cf954035dc39325a3a80656f8
Alyster Lara <alaramartnez@frba.utn.edu.ar>
2023-04-25 01:38:11 +0000

    tp0 terminado

--- client/src/client.c
+++ client/src/client.c
@@ -19,6 +19,8 @@ int main(void)
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
 
+	log_info(logger,"Soy un Logger") ;
+
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -27,7 +29,12 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
+	ip =config_get_string_value(config,"IP");
+	puerto = config_get_string_value(config,"PUERTO");
+	valor = config_get_string_value(config,"CLAVE");
+
 	// Loggeamos el valor de config
+	log_info(logger,"Lei el valor de la CLAVE %s, el PUERTO %s y el IP %s\n",valor, puerto, ip) ;
 
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
@@ -42,6 +49,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -56,12 +64,25 @@ t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
 
+	nuevo_logger = log_create("tp0.log","unLog", true ,LOG_LEVEL_INFO);
+
+	if(nuevo_logger == NULL) {
+		printf("No se pudo crear el logeer\n");
+		exit(1);
+	}
+
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
+	nuevo_config = config_create("./tp0.config");
+
+	if(nuevo_config == NULL) {
+		printf("No se pudo leer la config\n");
+		exit(2);
+	}
 
 	return nuevo_config;
 }
@@ -74,27 +95,45 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
+	while(strcmp(leido,"\0") != 0) {
+		leido = readline("");
+		log_info(logger,"Lei las lineas %s\n", leido);
+	}
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	free(leido);
 }
 
 void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
+		while(strcmp(leido,"") != 0) {
+			leido = readline("> ");
+			int tamanio = strlen(leido) + 1;
+			agregar_a_paquete(paquete, leido, tamanio);
+		}
 
+		enviar_paquete(paquete, conexion);
+		// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+		eliminar_paquete(paquete);
 
-	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
 }
 
-void terminar_programa(int conexion, t_log* logger, t_config* config)
-{
+void terminar_programa(int conexion, t_log* logger, t_config* config) {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	if(logger != NULL){
+		log_destroy(logger);
+	}
+
+	if(config != NULL){
+		config_destroy(config);
+	}
+
+	liberar_conexion(conexion);
 }
--- server/src/utils.c
+++ server/src/utils.c
@@ -5,9 +5,6 @@ t_log* logger;
 int iniciar_servidor(void)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
-
-	int socket_servidor;
 
 	struct addrinfo hints, *servinfo, *p;
 
@@ -19,10 +16,23 @@ int iniciar_servidor(void)
 	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
 
 	// Creamos el socket de escucha del servidor
+	int socket_servidor = socket(servinfo->ai_family,
+								servinfo->ai_socktype,
+								servinfo->ai_protocol);
 
 	// Asociamos el socket a un puerto
 
+
+	bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen);
+
+	//TODO
+//	if(	bind(socket_servidor,(void*) &servinfo,sizeof(servinfo))!= 0){
+//			perror("Fallo el bind");
+//			return 1;
+//		}
+
 	// Escuchamos las conexiones entrantes
+	listen(socket_servidor, SOMAXCONN);
 
 	freeaddrinfo(servinfo);
 	log_trace(logger, "Listo para escuchar a mi cliente");
@@ -33,10 +43,10 @@ int iniciar_servidor(void)
 int esperar_cliente(int socket_servidor)
 {
 	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
 
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
+	//TODO
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
 	log_info(logger, "Se conecto un cliente!");
 
 	return socket_cliente;
